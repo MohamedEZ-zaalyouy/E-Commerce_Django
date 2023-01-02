321ca47e6a4ca948a7e2f3da96e51bbc5853bf67
MohamedEZ-zaalyouy <mohamedezzaalyouy.98@gmail.com>
2023-01-02 11:40:25 +0100

    3 commit

---
+++ product/admin.py
@@ -0,0 +1,25 @@
+from django.contrib import admin
+from .models import Category, Product
+
+
+# ===========================
+# setting your admin models.
+# ===========================
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['title', 'parent', 'status']
+    list_filter = ['title', 'status']
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['title', 'category', 'status']
+    list_filter = ['category']
+   # readonly_fields = ('image_tag',)
+   # inlines = [ProductImageInline,ProductVariantsInline,ProductLangInline]
+    prepopulated_fields = {'slug': ('title',)}
+
+
+# ===========================
+# Register your models here.
+# ===========================
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Product, ProductAdmin)
