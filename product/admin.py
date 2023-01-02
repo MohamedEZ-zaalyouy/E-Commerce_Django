b8ad76dcc065977b03f67a4483762ff61c9f90ac
MohamedEZ-zaalyouy <mohamedezzaalyouy.98@gmail.com>
2023-01-02 19:24:01 +0100

    Start class Images

--- product/admin.py
+++ product/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Category, Product
+from .models import Category, Product, Images
 
 
 # ===========================
@@ -23,3 +23,4 @@ class ProductAdmin(admin.ModelAdmin):
 # ===========================
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Product, ProductAdmin)
+admin.site.register(Images)
