27fe7e2e761c3ab0a15115b9466098bfe22ced1d
MohamedEZ-zaalyouy <mohamedezzaalyouy.98@gmail.com>
2023-01-31 23:25:09 +0100

    addtoshopcart

--- order/admin.py
+++ order/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from order.models import ShopCart
 
-# Register your models here.
+
+class ShopCartAdmin(admin.ModelAdmin):
+    list_display = ['product', 'user', 'quantity', 'price', 'amount']
+    list_filter = ['user']
+
+# =================================
+# Register your models order here.
+# =================================
+
+
+admin.site.register(ShopCart, ShopCartAdmin)
--- order/urls.py
+++ order/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('addtoshopcart/<int:id>', views.addtoshopcart, name='addtoshopcart'),
 ]
