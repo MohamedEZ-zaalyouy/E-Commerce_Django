7ffe19e91df4332c0f4b4eea819da8e00e74a228
MohamedEZ-zaalyouy <mohamedezzaalyouy.98@gmail.com>
2023-02-04 23:18:27 +0100

    Change User Password

--- user/urls.py
+++ user/urls.py
@@ -2,11 +2,11 @@ from django.urls import path
 from . import views
 
 
-#app_name = 'user'
+# app_name = 'user'
 urlpatterns = [
     path('', views.index, name='user_index'),
     path('update/', views.user_update, name='user_update'),
-    # path('password/', views.user_password, name='user_password'),
+    path('password/', views.user_password, name='user_password'),
     # path('orders/', views.user_orders, name='user_orders'),
     # path('orders_product/', views.user_order_product, name='user_order_product'),
     # path('orderdetail/<int:id>', views.user_orderdetail, name='user_orderdetail'),
